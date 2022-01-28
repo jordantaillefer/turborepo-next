96256e04b6411361c117dccbd802a4b118853043
jordan.taillefer <jordan.taillefer@accenture.com>
2022-01-28 17:58:54 +0100

    rm submodules

---
+++ apps/tutorial-next-1/cypress/integration/app.spec.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+describe('Navigation', () => {
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+
+    // The new page should contain an h1 with "About page"
+    cy.get('h1').contains('Welcome to Next.js with Typescript!')
+  })
+})
+
+export {}
